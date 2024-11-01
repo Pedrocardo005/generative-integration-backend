74e5eeadd8645e24afcae26c6fa1594999d15ab0
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-11-01 10:44:23 -0300

    :white_check_mark: test: Cria teste da api de criar um chat
    By: Pedro da Conceicao
    Refs: #4

--- backend/gemini/tests.py
+++ backend/gemini/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+import json
+from unittest.mock import patch
+
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+
+
+def mock_gemini(text):
+    return 'Lorem ipsum'
+
+
+class ChatTests(APITestCase):
+    @patch('gemini.util.Util.gemini_integration', mock_gemini)
+    def test_create_chat(self):
+        url = reverse('gemini.chat')
+
+        data = {
+            'text': ''
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 400)
+
+        data = {
+            'text': 'abc'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 201)
+        response = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(response['text_input'], 'abc')
